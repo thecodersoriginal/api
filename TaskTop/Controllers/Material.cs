5c64828c95a669d2eb69b8c99f6d1f21d2eb0097
thecodersoriginal <thecodersoriginal@gmail.com>
2018-10-13 15:07:05 -0300

    commit material

---
+++ TaskTop/Controllers/Material.cs
@@ -0,0 +1,69 @@
+using APICore.Model.Selection;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TaskTop.Authorization.Model;
+using TaskTop.DTO;
+using TaskTop.Model;
+using TaskTop.Utils;
+
+namespace TaskTop.Controllers
+{
+    [Authorize]
+    public class MaterialController : EntityController<Material, MaterialDTO, int>
+    {
+        public MaterialController(TaskTopContext ctx, IMapper mapper) : base(ctx, mapper) { }
+
+        public override Expression<Func<Material, int>> GetInternalId => ent => ent.Id;
+        public override Expression<Func<MaterialDTO, int>> GetExternalId => ent => ent.id;
+
+        public override IQueryable<Material> FilterQuery(IQueryable<Material> query)
+        {
+            switch(Operator.Type)
+            {
+                case UserType.Funcionario:
+                case UserType.Supervisor:
+                case UserType.Estoque:
+                    return query.Where(a => (a.Origem == Operator.Id) || (a.Destino == Operator.Id));
+                default:
+                    return query;
+            }
+        }
+
+        public override async Task<Alerta> BeforeAdd(Alert sendedData)
+        {
+            var alerta = await base.BeforeAdd(sendedData);
+            alerta.VisualizadaEm = null;
+
+            return alerta;
+        }
+
+        public override Task<IActionResult> GetAll([FromBody] APIQuery query) => _GetAll(query);
+        public override Task<IActionResult> GetByKey(int? id) => _GetByKey(id);
+        public override Task<IActionResult> Add([FromBody] MaterialDTO ent) => _Add(ent);
+
+        [HttpPost]
+        public async Task<IActionResult> Read(int? id)
+        {
+            var alert = await InitialQuery.SingleOrDefaultAsync(a => a.Id == id);
+
+            if (alert == null)
+                return NotFound();
+
+            alert.VisualizadaEm = DateTime.UtcNow;
+
+            DbContext.Entry(alert).State = EntityState.Modified;
+
+            await DbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+    }
+}
+
---
+++ TaskTop/DTO/MaterialDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTop.DTO
+{
+    public class MaterialDTO
+    {
+        public int id;
+        public string description;
+        public string actualQuantity;
+    }
+}
